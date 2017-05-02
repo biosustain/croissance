37192cccb1b0f48ea080064bf48ce35f4ce28b4f
Lars Schöning <lars@lyschoening.de>
2017-05-02 12:21:47 +0200

    fix: explicit outlier_mask

--- croissance/estimation/outliers.py
+++ croissance/estimation/outliers.py
@@ -17,7 +17,7 @@ def remove_outliers(series, window=30, std=2):
 
     values = with_overhangs(series.values, window)
     outliers = abs(values - values.rolling(window=window, center=True).median()) < values.rolling(window=window, center=True).std() * std
-    outlier_mask = outliers[window:-window]
+    outlier_mask = outliers[window:-window].values
 
     outliers = pandas.Series(data=series.values[~outlier_mask], index=series.index[~outlier_mask])
     series = pandas.Series(data=series.values[outlier_mask], index=series.index[outlier_mask])

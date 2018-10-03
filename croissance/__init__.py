5435fbcde137e2ddbff20c2ad1b82c0fb8c78fc0
meono <emoz@biosustain.dtu.dk>
2018-10-03 12:44:45 +0200

    fix: empty wells will return None when using the package through python.

--- croissance/__init__.py
+++ croissance/__init__.py
@@ -3,6 +3,8 @@ from croissance.estimation import Estimator
 
 def process_curve(curve: 'pandas.Series', **kwargs):
     estimator = Estimator(**kwargs)
+    if curve.empty:
+        return None
     return estimator.growth(curve)
 
 
